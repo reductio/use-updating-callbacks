276963e94d0216874d48505c1e468656fc78f2f6
Janko Timmermann <janko@timmermann.online>
2020-09-09 15:10:45 +0200

    updated rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,6 @@
 import typescript from "rollup-plugin-typescript2";
 import { terser } from "rollup-plugin-terser";
+import bundleSize from "rollup-plugin-bundle-size";
 
 export default {
   input: "index.ts",
@@ -7,7 +8,19 @@ export default {
   output: {
     file: "dist/index.js",
     format: "cjs",
-    plugins: [terser()],
+    plugins: [
+      terser({
+        output: {
+          comments: false,
+        },
+        compress: {
+          booleans_as_integers: true,
+          drop_console: true,
+          unsafe: true,
+        },
+      }),
+      bundleSize(),
+    ],
   },
   external: ["react"],
 };
